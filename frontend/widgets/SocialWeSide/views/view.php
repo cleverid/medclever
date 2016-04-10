5d10951ae48d8889bd7a43655089af8f3259c694
eawapownikov <eawapownikov@gmail.com>
2016-04-10 11:13:58 +0300

    Sicial icon maked for ok on main page.

--- frontend/widgets/SocialWeSide/views/view.php
+++ frontend/widgets/SocialWeSide/views/view.php
@@ -7,8 +7,8 @@
         <a href="https://www.facebook.com/%D0%A0%D0%B0%D0%B7%D1%83%D0%BC%D0%BD%D0%B0%D1%8F-%D0%BC%D0%B5%D0%B4%D0%B8%D1%86%D0%B8%D0%BD%D0%B0-998852680154157/" target="_blank">
             <i class="icon-social-fb"></i>
         </a>
-        <?if(0):?>
-            <i class="icon-social-tw"></i>
-        <?endif;?>
+        <a href="http://ok.ru/group/53047103062188" target="_blank">
+            <i class="icon-social-ok"></i>
+        </a>
     </div>
 </div>
