7c0110583e5df855b6539f87fc626ed4b57191fa
eawapownikov <eawapownikov@gmail.com>
2018-02-18 01:09:14 +0300

    Fix. 4

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -1,11 +1,13 @@
 <?php
+
+$MYSQL_HOST = getenv("MYSQL_HOST");
 return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=medclever',
-            'username' => 'medclever',
-            'password' => 't2bmZZdjnBZ2rWfV',
+            'dsn' => "mysql:host=$MYSQL_HOST;dbname=medclever",
+            'username' => getenv("MYSQL_USER"),
+            'password' => getenv("MYSQL_PASSWORD"),
             'charset' => 'utf8',
         ],
         'mailer' => [
